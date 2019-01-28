726c26d471b23d2f3d0eb5e97e7d0a1caf7f4e16
Francisco Pozo <francisco.pozo@mdh.se>
2019-01-28 15:44:37 +0100

    Started the network package for the scheduler

--- SelfHealingProtocol/Scheduler/main.c
+++ SelfHealingProtocol/Scheduler/main.c
@@ -1,15 +1,19 @@
-//
-//  main.c
-//  SelfHealingProtocol
-//
-//  Created by Francisco Pozo on 2018-11-20.
-//  Copyright © 2018 Francisco Pozo. All rights reserved.
-//
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                                                                     *
+ *  Main.c                                                                                                             *
+ *  SelfHealingProtocol Scheduler                                                                                      *
+ *                                                                                                                     *
+ *  Created by Francisco Pozo on 28/01/19.                                                                             *
+ *  Copyright © 2019 Francisco Pozo. All rights reserved.                                                              *
+ *                                                                                                                     *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 #include <stdio.h>
+#include "Scheduler/Network.h"
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
+    read_network_xml("/Users/fpo01/OneDrive - Mälardalens högskola/PhD Folder/Software/SelfHealingProtocol/SelfHealingProtocol/Files/Networks/network.xml");
     return 0;
 }
